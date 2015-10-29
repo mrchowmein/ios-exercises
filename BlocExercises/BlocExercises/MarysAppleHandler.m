74b8ad333738f2338a08cd6daba5e980657a26d5
Jason Chan <jasonfchan@gmail.com>
2015-10-28 18:24:21 -0700

    assignment 11 completed

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -13,20 +13,37 @@
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
     NSString *itemToReturn;
 
-    /* WORK HERE */
+    if (dollars <= 4)
+    {
+        itemToReturn = @"get out of my store";
+    }
+    
+    if (dollars == 5){
+        itemToReturn =  @"have some gum";
+    }
+    
+    if (dollars == 6){
+        itemToReturn =  @"have an apple";
+    }
+    
+    if (dollars >= 1000){
+        itemToReturn =  @"have an Apple computer";
+    }
+    
+    if (dollars >= 1000000000){
+        itemToReturn =  @"have The Big Apple";
+    }
+    
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
 
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
-    /* WORK HERE */
-
-    NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
+
+    NSUInteger cost = (self.getsDiscount) ? 18 : 24;
+    
     
     return cost;
 }
