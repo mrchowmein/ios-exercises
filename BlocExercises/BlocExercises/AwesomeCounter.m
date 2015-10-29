6a2966107471551f5d5a8f24eec47185db2e4da0
Jason Chan <jasonfchan@gmail.com>
2015-10-28 19:44:48 -0700

    assignment 12 completed

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,29 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+    
+    NSMutableString *inbetween = [[NSMutableString alloc]init];
+    
+    if (number <= otherNumber){
+       
+    while (number <= otherNumber)
+    {
+        [inbetween appendString:[NSString stringWithFormat:@"%ld", number]];
+        number = number + 1;
+    }
+    }
+    
+        else
+    {
+        while(number >= otherNumber){
+        [inbetween appendString:[NSString stringWithFormat:@"%ld", otherNumber]];
+        otherNumber = otherNumber + 1;
+        
+    }
+    
+    }
+    NSLog(@"%@", inbetween);
+    return inbetween;
 }
 
 @end
